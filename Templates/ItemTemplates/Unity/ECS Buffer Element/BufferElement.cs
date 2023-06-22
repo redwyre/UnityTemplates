cbcadc09df74bd3bc8dd0dc090afdf6045e4f24b
Simon <redwyre@outlook.com>
2023-06-22 17:56:32 +1000

    unity dots item templates

---
+++ Templates/ItemTemplates/Unity/ECS Buffer Element/BufferElement.cs
@@ -0,0 +1,12 @@
+using System;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Core;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+public struct $fileinputname$ : IBufferElementData
+{
+    public int Value;
+}
