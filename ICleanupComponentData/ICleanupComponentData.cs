2bab07fdb838baefc2cbb1f5469e8f2117014595
Simon <redwyre@users.noreply.github.com>
2023-07-15 07:55:01 +1000

    more files

---
+++ ICleanupComponentData/ICleanupComponentData.cs
@@ -0,0 +1,12 @@
+using System;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Core;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+public struct $fileinputname$ : ICleanupComponentData
+{
+    public int Value;
+}
