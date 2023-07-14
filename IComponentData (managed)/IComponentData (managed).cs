3ae287a0b6b07a687276862a18ca786a6dd6d4fd
Simon <redwyre@outlook.com>
2023-07-15 07:55:01 +1000

    more files

---
+++ IComponentData (managed)/IComponentData (managed).cs
@@ -0,0 +1,12 @@
+using System;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Core;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+public class $fileinputname$ : IComponentData
+{
+    public int Value;
+}
